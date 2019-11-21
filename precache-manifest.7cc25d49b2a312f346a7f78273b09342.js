d01294caf25816ba896537600d8373fb6d5940c8
lleonesouza <aliencode.host@gmail.com>
2019-11-21 11:41:09 -0300

    Updates

---
+++ precache-manifest.7cc25d49b2a312f346a7f78273b09342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae10851c288f90f622c67c34370c376",
+    "url": "/castor/index.html"
+  },
+  {
+    "revision": "456f44652096bfa73066",
+    "url": "/castor/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f105fcce5aca850a4ed7",
+    "url": "/castor/static/js/2.b5b7facb.chunk.js"
+  },
+  {
+    "revision": "456f44652096bfa73066",
+    "url": "/castor/static/js/main.9e640722.chunk.js"
+  },
+  {
+    "revision": "48aa82b3059dfdb05e7c",
+    "url": "/castor/static/js/runtime-main.daa842df.js"
+  },
+  {
+    "revision": "a1f35522aa79c820d0f76f6aec093251",
+    "url": "/castor/static/media/castor.a1f35522.svg"
+  }
+]);
